8c7c7bab4cbc1796d7b313f9aa17282d533a5560
MrKevinOConnell <kevinoconnell42@gmail.com>
2020-02-10 16:47:52 -0600

    Initial Python file
    Random variables for weather made, print statement established with weather stations

---
+++ weatherStation.py
@@ -0,0 +1,38 @@
+"""given a beginning number and end number,
+ prints out the names of weather stations, and assigns them with a unique weather type
+    """
+
+import random
+
+s = 100
+e = 0
+b = ""
+
+def makeBiome():
+    
+
+
+def randomVariables(low, high, n):
+    for x in range(n):
+        temp = random.randint(low, high)
+
+        return temp
+
+
+while s > e:
+    b = e + 1
+    c = str(b)
+    rand = str(randomVariables(70, 90, s))
+    f = "Number weather station is " + c + " " + rand
+
+
+    print(f)
+
+
+    e += 1
+
+
+
+
+
+
