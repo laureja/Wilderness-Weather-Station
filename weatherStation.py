0255c7e01b3798775dc2ad3ed231699f7ed2e20c
MrKevinOConnell <kevinoconnell42@gmail.com>
2020-02-19 16:57:27 -0600

    WeatherStation Python
    made a class that generates data and converts the file to a json

--- weatherStation.py
+++ weatherStation.py
@@ -3,36 +3,66 @@
     """
 
 import random
-
+import json
 s = 100
 e = 0
 b = ""
+biome = ["rainforest","tundra","desert","plains"]
+class makeBiome:
+    def __init__(self,):
+        self.biome = random.choice(biome)
+        self.temp = []
+        self.hum = []
+        self.wind = []
 
-def makeBiome():
-    
+    def makeTemp(self):
+        if(self.biome == "rainforest"):
+            for x in range(50):
+                self.temp.append(random.randint(70,85))
+        elif (self.biome == "tundra"):
+            for x in range(50):
+                self.temp.append(random.randint(-15, 20))
+        elif (self.biome == "desert"):
+            for x in range(50):
+                self.temp.append(random.randint(90, 110))
+        elif (self.biome == "plains"):
+            for x in range(50):
+                self.temp.append(random.randint(45, 69))
+
+    def makeHum(self):
+        if (self.biome == "rainforest"):
+            for x in range(50):
+                self.hum.append(random.randint(77, 88))
+        elif (self.biome == "tundra"):
+            for x in range(50):
+                self.hum.append(random.randint(1, 15))
+        elif (self.biome == "desert"):
+            for x in range(50):
+                self.hum.append(random.randint(20,30))
+        elif (self.biome == "plains"):
+            for x in range(50):
+                self.hum.append(random.randint(50, 65))
+
+    def makeWind(self):
+        if (self.biome == "rainforest"):
+            for x in range(50):
+                self.wind.append(random.randint(3, 10))
+        elif (self.biome == "tundra"):
+            for x in range(50):
+                self.wind.append(random.randint(50,200))
+        elif (self.biome == "desert"):
+            for x in range(50):
+                self.wind.append(random.randint(4, 11))
+        elif (self.biome == "plains"):
+            for x in range(50):
+                self.wind.append(random.randint(10, 30))
 
 
-def randomVariables(low, high, n):
-    for x in range(n):
-        temp = random.randint(low, high)
+biome = makeBiome()
+biome.makeTemp()
+biome.makeHum()
+biome.makeWind()
 
-        return temp
+jsonStr = json.dumps(biome.__dict__)
 
-
-while s > e:
-    b = e + 1
-    c = str(b)
-    rand = str(randomVariables(70, 90, s))
-    f = "Number weather station is " + c + " " + rand
-
-
-    print(f)
-
-
-    e += 1
-
-
-
-
-
-
+print(jsonStr)
