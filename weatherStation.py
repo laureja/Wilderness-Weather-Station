9b4b387eb9c4007cc58e012201984ff01c02c527
MrKevinOConnell <kevinoconnell42@gmail.com>
2020-02-25 20:27:36 -0600

    finished weather station creation

--- weatherStation.py
+++ weatherStation.py
@@ -1,87 +1,99 @@
+"""given a beginning number and end number,
+ prints out the names of weather stations, and assigns them with a unique weather type
+    """
+
 import random
 import json
-
+s = 100
+e = 0
+b = ""
 biome = ["rainforest","tundra","desert","plains"]
-
 class makeWeatherStation:
-    def __init__(self):
+    def __init__(self,):
         self.biome = random.choice(biome)
         self.coordinates = []
-        self.temp = []
-        self.hum = []
-        self.wind = []
+        self.temp = 0
+        self.hum = 0
+        self.wind = 0
         self.battery = 100
-
+        self.name = ""
     def makeTemp(self):
         if(self.biome == "rainforest"):
-            for x in range(50):
-                self.temp.append(random.randint(70,85))
+          self.temp = random.randint(70,85)
         elif (self.biome == "tundra"):
-            for x in range(50):
-                self.temp.append(random.randint(-15, 20))
+             self.temp = random.randint(-15, 20)
         elif (self.biome == "desert"):
-            for x in range(50):
-                self.temp.append(random.randint(90, 110))
+             self.temp = random.randint(90, 110)
         elif (self.biome == "plains"):
-            for x in range(50):
-                self.temp.append(random.randint(45, 69))
+            self.temp = random.randint(45, 69)
 
     def makeHum(self):
         if (self.biome == "rainforest"):
-            for x in range(50):
-                self.hum.append(random.randint(77, 88))
+
+                self.hum = random.randint(77, 88)
         elif (self.biome == "tundra"):
-            for x in range(50):
-                self.hum.append(random.randint(1, 15))
+                self.hum = random.randint(1, 15)
         elif (self.biome == "desert"):
-            for x in range(50):
-                self.hum.append(random.randint(20,30))
+                self.hum  = random.randint(20,30)
         elif (self.biome == "plains"):
-            for x in range(50):
-                self.hum.append(random.randint(50, 65))
+                self.hum =random.randint(50, 65)
 
     def makeWind(self):
         if (self.biome == "rainforest"):
-            for x in range(50):
-                self.wind.append(random.randint(3, 10))
+                self.wind = random.randint(3, 10)
         elif (self.biome == "tundra"):
-            for x in range(50):
-                self.wind.append(random.randint(50,200))
+              self.wind = random.randint(50,200)
         elif (self.biome == "desert"):
-            for x in range(50):
-                self.wind.append(random.randint(4, 11))
+                self.wind = random.randint(4, 11)
         elif (self.biome == "plains"):
-            for x in range(50):
-                self.wind.append(random.randint(10, 30))
+                self.wind = random.randint(10, 30)
+
     def makeLongAndLat(self):
         if (self.biome == "rainforest"):
-            self.coordinates.append(random.uniform(76.234587, 72.458019))
-            self.coordunates.append(random.uniform(-47.421122, -33.847574))
+            self.coordinates = [random.uniform(76.234587, 72.458019),random.uniform(-47.421122, -33.847574)]
         elif (self.biome == "tundra"):
-            self.coordinates.append(random.uniform(76.234587, 72.458019))
-            self.coordinates.append(random.uniform(-47.421122, -33.847574))
+            self.coordinates = [random.uniform(76.234587, 72.458019),random.uniform(-47.421122, -33.847574)]
+
         elif (self.biome == "desert"):
-            self.coordinates.append(random.uniform(37.637423,40.723223))
-            self.coordinates.append(random.uniform(-119.774298, -116.331553))
+            self.coordinates = [random.uniform(37.637423,40.723223),random.uniform(-119.774298, -116.331553)]
+
         elif (self.biome == "plains"):
-            self.coordinates.append(random.uniform(42.193239,44.211069))
-            self.coordinates.append(random.uniform(-109.993484, -105.423172))
+            self.coordinates = [random.uniform(42.193239,44.211069),random.uniform(-109.993484, -105.423172)]
+
     def simulateDay(self,days):
         for x in range(days):
             self.makeTemp()
             self.makeWind()
             self.makeHum()
             self.battery -= random.randint(1,5)
-            
-    
+jsonStr = ""
+stations = []
+weatherStation = makeWeatherStation()
+weatherStation2 = makeWeatherStation()
+weatherStation3 = makeWeatherStation()
+weatherStation4 = makeWeatherStation()
+weatherStation5 = makeWeatherStation()
+stations.append(weatherStation)
+stations.append(weatherStation2)
+stations.append(weatherStation3)
+stations.append(weatherStation4)
+stations.append(weatherStation5)
+c = 1
+for i in stations:
+
+     i.name = "weatherStation " + str(c)
+     c = c+1
+for x in stations:
+
+    x.makeTemp()
+    x.makeHum()
+    x.makeWind()
+    x.makeLongAndLat()
+    jsonStr += "{\"type\": \"Feature\", \"geometry\":{\"type\": \"Point\", \"coordinates\":" + str(x.coordinates) + "},\"properties\": " + json.dumps(x.__dict__)
+    jsonStr += "\n"
 
 
-biome = makeWeatherStation()
-biome.makeTemp()
-biome.makeHum()
-biome.makeWind()
-biome.makeLongAndLat()
-jsonStr = "{\"type\": \"Feature\", \"geometry\":{\"type\": \"Point\", \"coordinates\":" + str(biome.coordinates) + "},\"properties\": " + json.dumps(biome.__dict__)
 
-
-print(jsonStr)
+f = open("stationCreation.json","w+")
+f.write(jsonStr)
+f.close()
